6f92e434c74481653e664147a48e6f04e21b0a83
Alyhrvds <163549969+Alyhrvds@users.noreply.github.com>
2024-06-24 12:42:53 -0300

    adsiconando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+//tudo que quiser fazer só uma vez
+function setup() {
+    createCanvas(1000, 300);
+    background("purple");
+  }
+  
+  //desenhar
+  function draw() {
+    fill("pink"); //cor do retangulo
+    stroke("red"); //cor da linha
+  
+    //console.log(mouseIsPressed)
+  
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 30, 60); //retangulo
+    }
+  }
+  
